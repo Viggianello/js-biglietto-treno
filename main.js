b2149f9854e0d5f3149f32fbee58b282885f12f2
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-20 18:57:39 +0200

    esercizio extra ultimato con tutte i possibili check sui dati da inserire

--- main.js
+++ main.js
@@ -6,22 +6,40 @@
 
 // chiedere all utente la sua età
 var eta = prompt('Qual è la sua età:', '30');
+
 // chiedere all utente il numero di km che vuole percorrere
 var km = prompt('Quanti chilometri vuole percorrere:' ,'20');
+
 // costo biglietto
 var biglietto = (km * 0.21);
-// calcolare per poi applicare lo sconto corretto in base all età e visualizzare questo output
-if (eta < 18) {
+
+// 1)check dei dati inseriti 2)calcolare  per poi applicare lo sconto corretto in base all età 3)visualizzare questo output
+// valutare se l'utente ha messo un valore numerico
+if (isNaN(eta) == true || isNaN(km) == true) {
+document.getElementById('stampa').innerHTML='perfavore non mi prenda in giro...inserisca dei valori numerici';
+}
+
+// valutare se l'utente ha messo un valore numerico non negativo
+else if (eta < 0 == true || km < 0 == true) {
+document.getElementById('stampa').innerHTML='perfavore non mi prenda in giro...non può aver una età o percorrere una distanza negativa';
+}
+
+// valutare se l'utente ha messo un valore numerico non negativo non decimale
+else if (eta % 1 != 0 || km % 1 !=0) {
+    document.getElementById('stampa').innerHTML='perfavore non mi prenda in giro...non può aver una età o percorre una distanza decimale';
+}
+
+else if (eta < 18) {
     var sconto_minorenni = ((biglietto / 100) * 20);
     var biglietto_scontato_minorenni = (biglietto - sconto_minorenni);
     // console.log ('Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ' + biglietto_scontato_minorenni.toFixed(2) + '€');
-    document.getElementById('stampa').innerHTML='Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ' + biglietto_scontato_minorenni.toFixed(2) + '€';
+    document.getElementById('stampa').innerHTML='Essendo minorenne il suo biglietto usufruisce di uno sconto del 20% venendo così a costare: ' + biglietto_scontato_minorenni.toFixed(2) + '€';
 
 } else if (eta > 65) {
     var sconto_anziani = ((biglietto / 100 ) * 40);
     var biglietto_scontato_anziani = (biglietto - sconto_anziani);
     // console.log ('Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ' + biglietto_scontato_anziani.toFixed(2) + '€');
-    document.getElementById('stampa').innerHTML='Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ' + biglietto_scontato_anziani.toFixed(2) + '€';
+    document.getElementById('stampa').innerHTML='Essendo over 65 di età il suo biglietto usufruisce di uno sconto del 40% venendo così a costare: ' + biglietto_scontato_anziani.toFixed(2) + '€';
 
 }
 else {
@@ -38,19 +56,6 @@ else {
 // console.log (biglietto('€'));
 // console.log (biglietto_scontato_minorenni('€'));
 // console.log (biglietto_scontato_anziani('€'));
-
-if (isNaN(eta) == true || isNaN(km) == true) {
-console.log ('perfavore non prendermi in giro...inserisci dei valori numerici');
-}
-
-else if (eta < 0 == true || km < 0 == true) {
-console.log ('perfavore non prendermi in giro...non puoi aver una età o percorrere una distanza negativa ');
-}
-
-else if (eta % 1 != 0 || km % 1 !=0) {
-    console.log ('perfavore non prendermi in giro...non puoi aver una età o percorre decimale');
-}
-
-else {
-    console.log ('Bravo!!! hai inserito dei valori numerici');
-}
+// else {
+//     console.log ('Bravo!!! hai inserito dei valori numerici');
+// }
