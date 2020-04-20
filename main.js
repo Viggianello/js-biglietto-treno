047283ed15d6d111e840921eb7b660594ea33a43
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-20 16:01:53 +0200

    fiorellini

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ var biglietto = (km * 0.21);
 if (eta < 18) {
     var sconto_minorenni = ((biglietto / 100) * 20);
     var biglietto_scontato_minorenni = (biglietto - sconto_minorenni);
-    console.log (('Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ') + biglietto_scontato_minorenni);
+    console.log (('Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ') + biglietto_scontato_minorenni ('€'));
 } else if (eta > 65) {
     var sconto_anziani = ((biglietto / 100 )* 40);
     var biglietto_scontato_anziani = (biglietto - sconto_anziani);
-    console.log (('Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ') + biglietto_scontato_anziani);
+    console.log (('Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ') + biglietto_scontato_anziani ('€'));
 }
 else {
-    console.log (('Il costo del suo biglietto è: ') + biglietto);
+    console.log (('Il costo del suo biglietto è: ') + biglietto ('€'));
 }
 
 console.log (eta);
