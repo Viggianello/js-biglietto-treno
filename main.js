95c159cfd7bf3b8bfb85fce27b5ba86729eb1261
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-20 16:37:14 +0200

    esercizio base finito

--- main.js
+++ main.js
@@ -14,20 +14,14 @@ var biglietto = (km * 0.21);
 if (eta < 18) {
     var sconto_minorenni = ((biglietto / 100) * 20);
     var biglietto_scontato_minorenni = (biglietto - sconto_minorenni);
-    // console.log ('Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ' + biglietto_scontato_minorenni.toFixed(2) + '€');
-    document.getElementById('stampa').innerHTML='Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ' + biglietto_scontato_minorenni.toFixed(2) + '€';
-
+    console.log (('Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ') + biglietto_scontato_minorenni ('€'));
 } else if (eta > 65) {
-    var sconto_anziani = ((biglietto / 100 ) * 40);
+    var sconto_anziani = ((biglietto / 100 )* 40);
     var biglietto_scontato_anziani = (biglietto - sconto_anziani);
-    // console.log ('Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ' + biglietto_scontato_anziani.toFixed(2) + '€');
-    document.getElementById('stampa').innerHTML='Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ' + biglietto_scontato_anziani.toFixed(2) + '€';
-
+    console.log (('Essendo over 65 di età il suo biglietto usufruisce di uno scondo del 40% venendo cosi a costare: ') + biglietto_scontato_anziani ('€'));
 }
 else {
-    // console.log ('Il costo del suo biglietto è: ' + biglietto + '€');
-    document.getElementById('stampa').innerHTML=''Il costo del suo biglietto è: ' + biglietto.toFixed(2) + '€';
-
+    console.log (('Il costo del suo biglietto è: ') + biglietto ('€'));
 }
 
 // console.log (eta);
@@ -37,4 +31,4 @@ else {
 // console.log (biglietto_scontato_anziani('€'));
 
 
-document.getElementById('stampa').innerHTML='Essendo minorenne il suo biglietto usufruisce di uno scondo del 20% venendo cosi a costare: ' + biglietto_scontato_minorenni.toFixed(2) + '€';
+// document.getElementById('stampacolore').innerHTML='Ecco il tuo colore preferito:
